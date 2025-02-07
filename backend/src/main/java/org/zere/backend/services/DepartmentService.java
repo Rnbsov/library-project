42ca803a40dd88389b9f0e8cb0606a7d76018456
Rnbsov <rnbsov@gmail.com>
2025-02-08 00:20:22 +0500

    🎉 feat: Create Department service
    Changelog: feature

---
+++ backend/src/main/java/org/zere/backend/services/DepartmentService.java
@@ -0,0 +1,31 @@
+package org.zere.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zere.backend.models.Department;
+import org.zere.backend.repository.DepartmentRepository;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department createDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public Department getDepartmentById(Long id) {
+        return departmentRepository.findById(id).orElse(null);
+    }
+
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    public void deleteDepartment(Long id) {
+        departmentRepository.deleteById(id);
+    }
+}
