c5382bf0239e21b80767bf9211858d639d3bbb76
Rnbsov <rnbsov@gmail.com>
2025-02-08 00:24:14 +0500

    🎉 feat: Create EditionService
    Changelog: feature

---
+++ backend/src/main/java/org/zere/backend/services/EditionService.java
@@ -0,0 +1,35 @@
+package org.zere.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zere.backend.models.Edition;
+import org.zere.backend.repository.EditionRepository;
+
+import java.util.List;
+
+@Service
+public class EditionService {
+
+    @Autowired
+    private EditionRepository editionRepository;
+
+    public Edition createEdition(Edition edition) {
+        return editionRepository.save(edition);
+    }
+
+    public Edition getEditionById(Long id) {
+        return editionRepository.findById(id).orElse(null);
+    }
+
+    public List<Edition> getAllEditions() {
+        return editionRepository.findAll();
+    }
+
+    public List<Edition> findEditionsByYear(int year) {
+        return editionRepository.findByYearOfPublication(year);
+    }
+
+    public void deleteEdition(Long id) {
+        editionRepository.deleteById(id);
+    }
+}
